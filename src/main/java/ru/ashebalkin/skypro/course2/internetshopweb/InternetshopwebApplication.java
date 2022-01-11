5ee8a62c7fd03d1078cfeffd38a516de1da10081
antoshka <ashebalkin@diasoft.ru>
2022-01-11 22:28:39 +0300

    Initial commit

---
+++ src/main/java/ru/ashebalkin/skypro/course2/internetshopweb/InternetshopwebApplication.java
@@ -0,0 +1,13 @@
+package ru.ashebalkin.skypro.course2.internetshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetshopwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetshopwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ashebalkin/skypro/course2/internetshopweb/InternetshopwebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ashebalkin.skypro.course2.internetshopweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetshopwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
